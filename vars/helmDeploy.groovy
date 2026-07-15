e6694c78399b3d71f2e1495376e42c021c4f970d
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-15 15:49:47 +0530

    add jenkins shared lib

---
+++ vars/helmDeploy.groovy
@@ -0,0 +1,73 @@
+def call(Map configMap){
+    pipeline{
+        agent{
+            label 'AGENT-1'
+        }
+        environment{
+            appVersion=''
+            REGION='us-east-1'
+            ACC_ID='597819998113'
+            PROJECT='flower-store'
+            COMPONENT=configMap.get('component')
+        }
+        options{
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds()
+        }
+        parameters{
+            string(name: 'appVersion', description: 'Image version of the application')
+            choice(name: 'deploy_to', choices: ['dev', 'qa', 'prod'], description: 'Pick the Environment')
+        }
+        //Build
+        stages{         
+            stage('Deploy'){
+                steps{
+                    script{
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1'){
+                            sh """
+                                aws eks update-kubeconfig --region ${REGION} --name "${PROJECT}-${params.deploy_to}"
+                                kubectl get nodes
+                                kubectl apply -f namespace.yml
+                                sed -i "s/IMAGE_VERSION/${params.appVersion}/g" values-${params.deploy_to}.yaml
+                                helm upgrade --install $COMPONENT -f values-${params.deploy_to}.yaml -n store .
+                            """
+                        }                   
+                    }
+                }
+            }
+            stage('check status'){
+                steps{
+                    script{
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1'){
+                            def deploymentStatus=sh(returnStdout: true, script:"kubectl rollout status deployment/${COMPONENT} --timeout=30s -n store || echo FAILED").trim()
+                            if(deploymentStatus.contains("successfully rolled out")){
+                                echo "Deployment is success"
+                            }else{
+                                sh """
+                                helm rollback ${COMPONENT} -n store
+                                """
+                                def rollbackStatus=sh(returnStdout:true, script:"kubectl rollout status deployment/${COMPONENT} --timeout=30s -n store || echo FAILED").trim()
+                            if(rollbackStatus.contains("successfully rolled out")){
+                                echo "Deployment is failure,  Rollback success"
+                            }else{
+                            error "Deployment failure, Rollback failure"
+                            }
+                        }                   
+                    }
+                }
+            }
+        }
+        }
+        post{
+            always{
+            cleanWs()
+            }         
+            success{
+                echo ' BUILD SUCCESS'
+            }
+            failure{
+                echo ' BUILD FAILED'
+            }
+        }
+    }
+}
---
+++ vars/javaSharedJenkins.groovy
@@ -0,0 +1,147 @@
+def call(Map configMap){
+    pipeline{
+        agent{
+            label 'AGENT-1'
+        }
+        environment{
+            appVersion=''
+            REGION='us-east-1'
+            ACC_ID='597819998113'
+            PROJECT='flower-store'
+            COMPONENT=configMap.get('component')
+        }
+        options{
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds()
+        }
+        parameters{
+            booleanParam(name: 'deploy', defaultValue: false, description: 'togle this value')
+        }
+        //Build
+        stages{
+            stage('Read pom.xml'){
+                steps{
+                    script{
+                        def pom = readMavenPom()
+                        appVersion= pom.version
+                        echo "app version : ${appVersion}"
+                    }
+                }
+            }
+            stage('Build Application'){
+                steps{
+                    script{
+                        sh """
+                        mvn clean package -DskipTests
+                        """
+                    }
+                }
+            }
+            stage('unit testing'){
+                steps{
+                    script{
+                        sh """
+                        echo "Unit test cases"
+                        """
+                    }
+                }
+            }
+            stage('Sonar scan'){
+                environment{
+                    scannerHome = tool 'sonar 8.1'
+                }
+                steps{
+                    script{
+                    withSonarQubeEnv(installationName: 'sonar 8.1') {
+                    sh "${scannerHome}/bin/sonar-scanner"
+                }
+                    }
+                }
+            }
+            stage('Quality gates'){
+                steps{
+                    timeout(time: 1, unit: "HOURS"){
+                        waitForQualityGate abortPipeline :true
+                    }
+                }
+            }
+            stage('Trivy Dependency Scan') {
+                steps {
+                    script {
+                        sh '''
+                        trivy fs \
+                        --severity HIGH,CRITICAL \
+                        --ignore-unfixed \
+                        --exit-code 1 \
+                        .
+                        '''
+                    }
+                }
+            }
+            stage('Docker build'){
+                steps{
+                    script{
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1'){
+                            sh """                       
+                            docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                            """
+                        }                    
+                    }
+                }
+            }
+            stage('Trivy Image Scan'){
+                steps{
+                    script{
+                        sh """
+                        trivy image \
+                        --severity HIGH,CRITICAL \
+                        --ignore-unfixed \
+                        --exit-code 1 \
+                        ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                        """
+                    }
+                }
+            }
+            stage('Push image to ECR'){
+                steps{
+                    script{
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1'){
+                            sh """
+                            aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                            docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            """
+                        }                    
+                    }
+                }
+            }   
+            stage('Trigger Deploy'){
+                when {
+                    expression {params.deploy}
+                }
+                steps{
+                    script{
+                        build job :"${COMPONENT}-cd",
+                        parameters:[
+                            string(name: 'appVersion', value: "${appVersion}"),
+                            string(name: 'deploy_to', value: 'dev')
+                        ],
+                        propagate: false,
+                        wait: false                                    
+                    }
+                }
+            }
+        }
+
+        post{   
+            always{
+            cleanWs()
+            }       
+            success{
+                echo ' BUILD SUCCESS'
+            }
+            failure{
+                echo ' BUILD FAILED'
+            }
+        }
+    }
+}
