f9a54da3c8697158ec953ef87d5e528fce948ff2
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-16 16:51:00 +0530

    jenkins scan

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -28,7 +28,8 @@ def call(Map configMap){
                                 aws eks update-kubeconfig --region ${REGION} --name "${PROJECT}-${params.deploy_to}"
                                 kubectl get nodes
                                 kubectl apply -f namespace.yml
-                                sed -i "s/IMAGE_VERSION/${params.appVersion}/g" flowers-store-helm/api-gateway/values-${params.deploy_to}.yaml
+                                 cd ${COMPONENT}
+                                sed -i "s/IMAGE_VERSION/${params.appVersion}/g" values-${params.deploy_to}.yaml
                                 helm upgrade --install $COMPONENT -f values-${params.deploy_to}.yaml -n store .
                             """
                         }                   
